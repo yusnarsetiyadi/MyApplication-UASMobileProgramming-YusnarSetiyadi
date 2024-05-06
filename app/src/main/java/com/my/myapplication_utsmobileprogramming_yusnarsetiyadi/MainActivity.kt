dfe7dc59f6204b17bb301126fb3160b3d1349323
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2024-05-06 18:48:13 +0700

    initial commit

---
+++ app/src/main/java/com/my/myapplication_utsmobileprogramming_yusnarsetiyadi/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.my.myapplication_utsmobileprogramming_yusnarsetiyadi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.my.myapplication_utsmobileprogramming_yusnarsetiyadi.ui.theme.MyApplicationUTSMobileProgrammingYusnarSetiyadiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationUTSMobileProgrammingYusnarSetiyadiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Yusnar Setiyadi")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationUTSMobileProgrammingYusnarSetiyadiTheme {
+        Greeting("Yusnar Setiyadi")
+    }
+}
